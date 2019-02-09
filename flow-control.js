f716438ec3b0afa399501a1f9f310d00244c623b
Dylan Speight <d.speight92@gmail.com>
2019-02-09 00:47:15 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,7 +4,7 @@ function basicTeenager(age){if(age>12 && age<20){return "You are a teenager!"}}
 
 
 
-function teenager(age){if(age>12 && age<20){return 'You are a teenager!')}return 'You are not a teenager')}
+function teenager(age){}
 
 
 
